15f79714753fe2643eb699d9bf924a42bc3c7009
ranjithaponnana <ranjithaponnana2007@gmail.com>
2026-06-22 13:32:32 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+import Products from "./Products";
+import Login from "./Login";
+import Register from "./Register";
+import Cart from "./Cart";
+
+
+function App(){
+
+return(
+
+<div>
+
+<h1>
+Ranjitha E-Commerce Store
+</h1>
+
+
+<Register/>
+
+<hr/>
+
+<Login/>
+
+<hr/>
+
+<Products/>
+
+<hr/>
+
+<Cart/>
+
+
+</div>
+
+)
+
+}
+
+
+export default App;
